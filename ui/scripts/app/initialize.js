ef193bf989706fd9d9f86354afdadc2916907e33
ramit garg <ramit.garg@commonfloor.com>
2015-06-23 23:56:43 +0530

    image view updated

--- ui/scripts/app/initialize.js
+++ ui/scripts/app/initialize.js
@@ -4,6 +4,215 @@
 	var application = function() {
 
 		var data = {
+			dresses: [{
+				dress: {
+					name: "FLUTED HEM DRESS",
+					type: "Summer Dress"
+				},
+				image: "1.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}, {
+				dress: {
+					name: "PLEAT PRINTED DRESS",
+					type: "Summer Dress"
+				},
+				image: "2.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}, {
+				dress: {
+					name: "FLOWY SHIRT DRESS",
+					type: "Summer Dress"
+				},
+				image: "3.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}, {
+				dress: {
+					name: "DOUBLE LAYER DRESS",
+					type: "Summer Dress"
+				},
+				image: "4.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}, {
+				dress: {
+					name: "BEAD DETAIL DRESS",
+					type: "Summer Dress"
+				},
+				image: "5.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}, {
+				dress: {
+					name: "PLEATED DETAIL DRESS",
+					type: "Summer Dress"
+				},
+				image: "6.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}, {
+				dress: {
+					name: "PRINTED DRESS",
+					type: "Summer Dress"
+				},
+				image: "7.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}, {
+				dress: {
+					name: "PRINTED DRESS",
+					type: "Summer Dress"
+				},
+				image: "8.jpg",
+				price: {
+					amount: "39",
+					currency: {
+						symbol: "$"
+					}
+				},
+				sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+				colors: [{
+					name: "Blue",
+					class_name: "blue"
+				}, {
+					name: "Brown",
+					class_name: "brown"
+				}, {
+					name: "White",
+					class_name: "white"
+				}, {
+					name: "Green",
+					class_name: "green"
+				}]
+			}],
 			sizes: [{
 				id: "xs",
 				value: "XS"
@@ -137,28 +346,118 @@
 			categoriesUl.innerHTML = htmlContent + "</ul>";
 		};
 
+		/**
+		 * function to get HTML content to print colors
+		 * @param args JSON
+		 *		colors: Array
+		 * 		start Integer
+		 * 		end Integer
+		 *		noName boolean
+		 * @return String html representation
+		 */
+		var getColorsHtml = function(args) {
+
+			if (args == null || args.colors == null || args.start < 0 || args.start > args.end) {
+				return "";
+			}
+
+			var htmlContent = "";
+			for (var i = args.start; i < args.end; i++) {
+				htmlContent += "<li>" + 
+									"<a href=\"javascript:void(0)\">" + 
+										"<div class=\"" + args.colors[i].class_name + "\"></div>" + ((args.noName !== true)?args.colors[i].name:"") + 
+									"</a>" + 
+								"</li>";
+			}
+
+			return htmlContent;
+		};
+
 		var initColors = function(colors) {
 			var htmlContent = "<ul>";
 			var colorsUl = getDomElement("colors");
-			for (var i = 0; i < colors.length/2; i++) {
-				htmlContent += "<li>" + 
-									"<a href=\"javascript:void(0)\">" + 
-										"<div class=\"" + colors[i].class_name + "\"></div>" + colors[i].name + 
-									"</a>" + 
-								"</li>";
+			htmlContent += getColorsHtml({colors: colors, start: 0, end: colors.length/2}) + "</ul><ul>";
+			htmlContent += getColorsHtml({colors: colors, start: colors.length/2, end: colors.length}) + "</ul>";
+
+			colorsUl.innerHTML = htmlContent;
+		};
+
+		/**
+		 * function to create html elements to be printed in UI for dresses
+		 * @param dresses
+		 */
+		var initDresses = function(dresses) {
+
+			if (dresses == null) {
+				return;
 			}
 
-			htmlContent += "</ul><ul>"
+			var totalHTML = "<div class=\"row\">";
+			var dressContainer = getDomElement("dressContainer");
+			for (var i = 0; i < dresses.length; i++) {
+				totalHTML +=	"<div class=\"col s4\">" +
+									"<div class=\"row\">" +
+										"<div class=\"col s12\">" +
+											"<div class=\"card\" onmouseenter=\"onMouseOver({index: " + i + ", enter: true})\" onmouseleave=\"onMouseOver({index: " + i + "})\">" +
+												"<div class=\"card-image\">" +
+													"<img src=\"images/" + dresses[i].image + "\">" +
+												"</div>" +
+												"<div class=\"card-container\" id=\"cardContainer" + i + "\">" +
+													"<div class=\"card-content\">" +
+														"<p>" + dresses[i].dress.name + "</p>" +
+														"<p>" +
+															"<span class=\"dress-type\">" + dresses[i].dress.type + "</span>" +
+															"<span class=\"dress-price\">" + dresses[i].price.currency.symbol + dresses[i].price.amount + "</span>" +
+														"</p>" +
+													"</div>" +
+													"<div class=\"card-content\">" +
+														"<p>Sizes</p>" +
+														"<p>" +
+															"<span class=\"dress-type\">" + dresses[i].sizes.join(", ") + "</span>" +
+														"</p>" +
+													"</div>" +
+													"<div class=\"card-content colors\">" +
+														"<p>Colors</p>" +
+														"<p>" +
+															"<span class=\"dress-type\">" + getColorsHtml({colors: dresses[i].colors, start: 0, end: dresses[i].colors.length, noName: true}) + "</span>" +
+														"</p>" +
+													"</div>" +
+												"</div>" +
+											"</div>" +
+										"</div>" +
+									"</div>" +
+								"</div>";
 
-			for (var i = colors.length/2; i < colors.length; i++) {
-				htmlContent += "<li>" + 
-									"<a href=\"javascript:void(0)\">" + 
-										"<div class=\"" + colors[i].class_name + "\"></div>" + colors[i].name + 
-									"</a>" + 
-								"</li>";
+				if (i % 3 === 2) {
+					totalHTML += "</div><div class=\"row\">";
+				}
 			}
 
-			colorsUl.innerHTML = htmlContent + "</ul>";
+			if (i % 3 !== 0) {
+				totalHTML += "</div>";
+			}
+
+			dressContainer.innerHTML = totalHTML;
+		};
+
+		/**
+		 * function called when user hovers its mouse over a dress
+		 * @param args
+		 */
+		window.onMouseOver = function(args) {
+
+			if (args == null || args.index == null) {
+				return;
+			}
+
+			var cardContainer = document.getElementById("cardContainer" + args.index);
+			if (cardContainer != null) {
+				if (args.enter === true && cardContainer.className.indexOf("open") === -1) {
+					cardContainer.className += " open";
+				} else {
+					cardContainer.className = cardContainer.className.replace(/(?:^|\s)open(?!\S)/g, '');
+				}
+			}
 		};
 
 		return {
@@ -167,6 +466,7 @@
 				initCategories(data.categories);
 				initColors(data.colors);
 				initSizes(data.sizes);
+				initDresses(data.dresses);
 			}
 		}
 	}();
@@ -174,3 +474,4 @@
 	application.init();
 
 }());
+
